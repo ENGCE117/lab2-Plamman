7e64cfde8b73b7db5831c4955b753d94e075444c
Plamman <sunsurn_so67@live.rmutl.ac.th>
2025-01-28 19:40:43 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+void GetMatrix( int **value, int *row, int *col ) ;
+void PrintMatrix( int **value, int row, int col ) ;
+
 int main() {
-    //YOUR CODE HERE
+    int *data, m = 2, n = 3 ;
+    data = new int[ m * n ] ; 
+
+    GetMatrix( &data, &m, &n ) ;
+    PrintMatrix( &data, m, n ) ;
+
     return 0 ;
+
 }//end function
+
+void GetMatrix( int **value, int *row, int *col ) {
+ 
+ printf ("Enter the elements of the matrix (%d x %d):\n" , *row , *col ) ;
+
+ for ( int i=0 ; i < *row ; i++ ) {
+    for ( int j=0 ; j < *col ; j++ ) {
+      printf("Element at [%d][%d]: ", i, j) ;  
+      scanf ( "%d", &value[i * (*col) + j] ) ;
+
+    }
+  }
+}
+
+void PrintMatrix( int **value, int row, int col ) {
+
+ printf( "Matrix (%dx%d):\n" , row , col ) ;
+ 
+ for ( int i=0 ; i < row ; i++ ) {
+   for ( int j=0 ; j < col ; j++ ) {
+     printf ( "%d " , value[i * col + j] ) ;  
+
+   }
+   printf("\n");
+ }
+}
